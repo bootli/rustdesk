51182c9398725e81082f892f3d622f45e2f34596
bootli <106524425+bootli@users.noreply.github.com>
2025-09-16 18:34:19 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["lidesk.zjxs.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["lidesk.zjxs.top:21166"];
 pub const RS_PUB_KEY: &str = "7a7KgekOiKyvCowbcQ4KGPO3RjtvazxmVxlYWilYiQ4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
